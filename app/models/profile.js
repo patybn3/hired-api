ce95593624386744deb4a3f4be90acc399e804bf
patybn3 <paty.nigro@gmail.com>
2020-04-24 00:21:25 -0400

    AWS implemented and finalized, CRUD works
    Created a new key on profile schema for a file upload called profileUrl
    on profile_routes.js required the package multer, which was intalled
    created a multer destination called pictures (uploads previously created
    for testing, to be deleted). Required the file s3Upload from lib folder
    which was also created in this commit and holds AWS setup. On s3Upload
    required the packages aws-sdk, mimetypes, fs and path, aws-sdk was
    manually installed. Entered the bucket name from .env folder as well
    as AWS secret keys. Created a file called upload.js under the bin folder
    which holds the responses for s3Upload file. On profile_routes, edited
    POST and PATCH to implement AWS.

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const profileSchema = new mongoose.Schema({
+  profileUrl: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const profileRoutes = require('./app/routes/profile_routes')
+// const uploadRoutes = require('./app/routes/upload_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -67,6 +68,7 @@ app.use(requestLogger)
 app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(profileRoutes)
+// app.use(uploadRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
